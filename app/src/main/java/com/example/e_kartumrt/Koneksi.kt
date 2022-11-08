2f5a2cc8ee7209ae79024657217b3c7cb510c162
AzhureRaven <abrahamarthurfendy@gmail.com>
2022-11-08 19:53:42 +0700

    Test select users

--- app/src/main/java/com/example/e_kartumrt/Koneksi.kt
+++ app/src/main/java/com/example/e_kartumrt/Koneksi.kt
@@ -2,26 +2,30 @@ package com.example.e_kartumrt
 
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
+import java.sql.Connection
 import java.sql.DriverManager
 
 
 object Koneksi {
 
-    fun getConnection(){
+    fun getConnection():Connection{
 
         try {
             val policy = ThreadPolicy.Builder().permitAll().build()
             StrictMode.setThreadPolicy(policy)
             Class.forName("org.mariadb.jdbc.Driver")
-            val ip = IP.getIP() //pake ip laptop sekarang yg ipv4, cari di cmd ipconfig
+            val ip = IP.getIP() //pake ip laptop sekarang yg ipv4, cari di cmd ipconfig //copy IP.kt di discord, paste ke project sendiri, di gitignore itu biar gk tabrakan ip address masing-masing laptop
             //val ip = "localhost"
             val jdbcUrl = "jdbc:mariadb://$ip:3306/e_kartu_mrt"
             val connection = DriverManager.getConnection(jdbcUrl, "root", "")
             println(connection.isValid(0))
             println("Bekerja")
 
-            val query = connection.prepareStatement("SELECT * FROM rute")
-            val result = query.executeQuery()
+            //briefly cara pakenya
+            /*val query = connection.prepareStatement("SELECT * FROM rute")
+            val res = query.execute()//general purpose, return bool kalau berhasil
+            val res2 = query.executeUpdate()//INSERT UPDATE DELETE, return int
+            val result = query.executeQuery()//select
 
             while(result.next()){
 
@@ -29,11 +33,14 @@ object Koneksi {
 
                 val name = result.getString("nama_rute")
                 println("$id $name")
-            }
+            }*/
+
+            return connection
         }
         catch (e:Exception){
             println(e)
         }
+        return DriverManager.getConnection("", "root", "")
     }
 
 }
--- app/src/main/java/com/example/e_kartumrt/MainActivity.kt
+++ app/src/main/java/com/example/e_kartumrt/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.e_kartumrt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Koneksi.getConnection()
+        val query = Koneksi.getConnection().prepareStatement("select * from e_kartu") //gara-gara bisa return null, harus dikasih ?
+        val result = query.executeQuery()
+        val users = ArrayList<User>()
+        while(result.next()){
+            users.add(User(result.getInt("id_kartu"),
+                result.getString("nama_lengkap"),
+                result.getString("username"),
+                result.getString("password"),
+                result.getString("email"),
+                result.getString("tgl_lahir"),
+                result.getString("kelamin"),
+                result.getString("tgl_register"),
+                result.getDouble("saldo"),
+                result.getInt("status_kartu")
+                ))
+        }
+        Toast.makeText(this, users.toString(), Toast.LENGTH_SHORT).show()
+        println(users.toString())
     }
 }
